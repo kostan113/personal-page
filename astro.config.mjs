1d202d98ceb98c401155f7572170be549bd6ce83
kostan113 <kostan113@gmail.com>
2026-05-31 22:53:29 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://kostan113.github.io',
-  base: '/',
   output: 'static',
   build: {
     assets: '_assets'
