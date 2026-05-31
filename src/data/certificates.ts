02c296354060b8d7a60e89da355f56acb4378ae9
kostan113 <kostan113@gmail.com>
2026-05-31 23:16:18 +0300

    Update certificates.ts

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,6 +1,7 @@
 export interface Certificate {
   title: string;
   issuer: string;
+  year?: string;
   verifyUrl?: string;
   verifyLabel?: string;
 }
@@ -17,6 +18,18 @@ export const certificates: Certificate[] = [
     issuer: "Фирма «1С»",
     verifyUrl: "https://uc1.1c.ru/account/summary/?token=884a27f39846ff150f538d6e7fe000d1",
     verifyLabel: "Проверить сертификат"
+  },
+  {
+    title: "1С:Профессионал по 1С:Управлению торговлей 8",
+    issuer: "1С:Учебный центр №1",
+    year: "2026",
+    verifyUrl: "https://uc1.1c.ru/account/summary/?token=884a27f39846ff150f538d6e7fe000d1",
+    verifyLabel: "Проверить сертификат"
+  },
+  {
+    title: "Менеджер проектов",
+    issuer: "Яндекс Практикум",
+    year: "2024"
   }
 ];
 
